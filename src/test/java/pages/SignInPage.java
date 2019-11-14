4c1255d0c793e16b935f3e708efca15525d62680
Łukasz Solarski <lukasz.solar@gmail.com>
2019-11-14 22:06:31 +0100

    add signInPage and create class for RegisterPage

--- src/test/java/pages/SignInPage.java
+++ src/test/java/pages/SignInPage.java
@@ -1,5 +1,26 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import static utility.Screenshot.captureScreenshot;
+
 public class SignInPage extends BasePage {
-    //TODO
+
+    public SignInPage() {super();}
+
+    @FindBy(xpath = "//button[@id='SubmitCreate']")
+    private WebElement registerButton;
+
+    @FindBy(xpath = "//label[text()='Email address']/input")
+    private WebElement emailInput;
+
+    @Step
+    public RegisterPage openRegisterPage(String email) {
+        emailInput.sendKeys(email);
+        registerButton.click();
+        captureScreenshot();
+        return new RegisterPage();
+    }
 }
